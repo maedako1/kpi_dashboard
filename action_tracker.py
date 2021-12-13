12d5df34e7603b5b3b5dc7165e3c9aae9c4e0b36
Kota <koota.maeda@gmail.com>
2021-12-13 22:05:16 +0800

    4th commit

---
+++ action_tracker.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.title("Action Tracker")
+
+df = pd.read_excel("KPI_ACTION_TRACKER.xlsx", header=0)
+
+country_list = df["Country"].unique()
+
+# file_path = "C:\Users\koota\OneDrive\デスクトップ\KOTA\STREAMLIT\10 ACTION TRACKER"
+
+with open("KPI_ACTION_TRACKER.xlsx", 'rb') as my_file:
+    st.sidebar.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
+
+
+selected_country = st.sidebar.selectbox("Counry", country_list)
+# st.sidebar.file_uploader("File Upload")
+
+# selected_country = st.sidebar.multiselect(
+#     "Select your country",
+#     ["Japan", "Korea"],
+#     ["Japan", "Korea"]
+# )
+
+df_country = df[df["Country"] == selected_country]
+
+# st.write(df)
+st.table(df_country)
+
+
+
