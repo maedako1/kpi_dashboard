9e37c1cc0793a3f47ee6e3e4ad4c69fe171503a6
maedako1 <93229270+maedako1@users.noreply.github.com>
2021-12-16 21:46:36 +0800

    5th commit

--- action_tracker.py
+++ action_tracker.py
@@ -12,10 +12,11 @@ country_list = df["Country"].unique()
 # file_path = "C:\Users\koota\OneDrive\デスクトップ\KOTA\STREAMLIT\10 ACTION TRACKER"
 
 with open("KPI_ACTION_TRACKER.xlsx", 'rb') as my_file:
-    st.sidebar.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
+#    st.sidebar.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
+    st.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
 
 
-selected_country = st.sidebar.selectbox("Counry", country_list)
+selected_country = st.selectbox("Counry", country_list)
 # st.sidebar.file_uploader("File Upload")
 
 # selected_country = st.sidebar.multiselect(
