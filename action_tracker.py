cb696555da0747004105edcbdf4428ec6f32c353
maedako1 <93229270+maedako1@users.noreply.github.com>
2021-12-17 07:41:39 +0800

    Commit Dec 17th v1

--- action_tracker.py
+++ action_tracker.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+
 
 
 st.title("Action Tracker")
@@ -13,7 +15,6 @@ country_list = df["Country"].unique()
 
 with open("KPI_ACTION_TRACKER.xlsx", 'rb') as my_file:
     st.sidebar.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
-#    st.download_button(label = 'Download', data = my_file, file_name = 'KPI_ACTION_TRACKER.xlsx', mime = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')      
 
 
 selected_country = st.sidebar.selectbox("Counry", country_list)
@@ -27,8 +28,21 @@ selected_country = st.sidebar.selectbox("Counry", country_list)
 
 df_country = df[df["Country"] == selected_country]
 
-# st.write(df_country)
+fig = go.Figure(data=[go.Table(
+    columnorder = [1, 2, 3, 4, 5, 6, 7], 
+    columnwidth = [100, 150, 200, 150, 150, 500, 100],
+    header = dict(values=["Country", "What_Action Items","Why", "Who_Responsible","When_Due_date", "How",  "Progress"],
+                  fill_color="paleturquoise",
+                  align="left"),
+    cells=dict(values=[df_country.Country, df_country.What_Action_Items, df_country.Why, df_country.Who_Responsible, 
+                       df_country.When_Due_date, df_country.How, df_country.Progress],
+               align=["left", "left"],
+               font_size=13,
+               height=30)
+)])
+
+# st.write(df)
 st.table(df_country)
-# st.dataframe(df_country, width=900, height=2000)
+fig.show()
 
 
